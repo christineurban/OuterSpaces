d2b7fd611439196c8db7c115a6344d3a6791dabd
Christine Urban <christineurbansf@gmail.com>
2017-04-02 01:47:20 +0000

    WIP directions

--- static/scripts/myMapJS.js
+++ static/scripts/myMapJS.js
@@ -4,23 +4,27 @@
 // Initalize map of San Francisco //
 ////////////////////////////////////
 
-var map;
-var infoWindow;
 
 function initMap() {
-  var info = new google.maps.InfoWindow( {
+  
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  var infoWindow = new google.maps.InfoWindow( {
     maxWidth: 350
   });
   var sanFrancisco = {lat: 37.7599, lng: -122.440558};
-  var mapOptions = new google.maps.Map(document.getElementById("map"), {
+  var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 13,
     center: sanFrancisco
   });
-  map = mapOptions;
-  infoWindow = info;
+  directionsDisplay.setMap(map);
 
 
-  // Try HTML5 geolocation.
+
+  ///////////////////////////
+  // Try HTML5 geolocation //
+  ///////////////////////////
+
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       var pos = {
@@ -40,17 +44,52 @@ function initMap() {
     handleLocationError(false, infoWindow, map.getCenter());
   }
 
-}
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-      }
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+          infoWindow.setPosition(pos);
+          infoWindow.setContent(browserHasGeolocation ?
+                                'Error: The Geolocation service failed.' :
+                                'Error: Your browser doesn\'t support geolocation.');
+  }
 
 
-$(document).ready(function() {
+// TODO (Directions)
+// http://www.geocodezip.com/v3_simplemap_marker2markerdirections.html
+
+
+  ////////////////
+  // Directions //
+  ////////////////
+
+  function showDirections() {
+    console.log("showDirections");
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      var marker = new google.maps.Marker({
+        position: pos,
+        map: map
+      });
+      map.setCenter(pos);
+      var request = {
+       origin: pos,
+       destination: 'san francisco',
+       travelMode: google.maps.DirectionsTravelMode.DRIVING
+     };
+
+     directionsService.route(request, function (response, status) {
+       if (status == google.maps.DirectionsStatus.OK) {
+         directionsDisplay.setDirections(response);
+       }
+     });
+    });
+  }
+  
+  $(".directions").on("click", console.log("showDirections"));
+
+
 
   //////////////////////////////////////////////////////
   // Retrieve data from server, send to loop function //
@@ -73,7 +112,7 @@ $(document).ready(function() {
   ////////////////////////////////////////////////////////////
   // Iterate through all data points, send to plot function //
   ////////////////////////////////////////////////////////////
-  
+
   function loopDataTrucks(data) {
     for (var i = 0; i < data.length; i++) {
       plotDataTrucks(data[i]);
@@ -118,7 +157,7 @@ $(document).ready(function() {
         "<p>FOOD TRUCK</p>" +
         "<h3>" + title + "</h3>" + 
         "<p><button id='addToFavTrucks'>Add to Favorites</button> " +
-        "<button id='directionsTruck'>Directions</button></p>" +
+        "<button class='directions'>Directions</button></p>" +
         "<p><strong>Address:</strong> " + address + "</p>" +
         "<p><strong>Hours:</strong> " + schedule + "</p>" +
         "<p><strong>Cuisine:</strong> " + cuisine + "</p>" +
@@ -169,7 +208,7 @@ $(document).ready(function() {
         "<p><strong>POPOS</strong></p>" +
         "<h3>" + title + "</h3>" +
         "<p><button id='addToFavPopos'>Add to Favorites</button> " +
-        "<button id='directionsPopos'>Directions</button></p>" +
+        "<button class='directions'>Directions</button></p>" +
         "<p><strong>Address:</strong> " + address + "</p>" +
         "<p><strong>Hours:</strong> " + schedule + "</p>" +
         "<p><strong>Type:</strong> " + type + "</p>" +
@@ -203,7 +242,7 @@ $(document).ready(function() {
     // collect all markers in array
     poposMarkers.push(marker);
   }
-  
+
 
   function plotDataArt(data) {
     
@@ -222,7 +261,7 @@ $(document).ready(function() {
         "<p><strong>PUBLIC ART</strong></p>" +
         "<h3>" + title + "</h3>" +
         "<p><button id='addToFavArt'>Add to Favorites</button> " +
-        "<button id='directionsArt'>Directions</button></p>" +
+        "<button class='directions'>Directions</button></p>" +
         "<p><strong>Address:</strong> " + address + "</p>" +
         "<p><strong>Location:</strong> " + location + "</p>" +
         "<p><strong>Type:</strong> " + type + "</p>" +
@@ -264,17 +303,11 @@ $(document).ready(function() {
   // On checkbox change, call toggle function //
   /////////////////////////////////////////////
 
-  $("#truckMap").on("change", function() {
-      toggleTruckMarkers();
-  });
+  $("#truckMap").on("change", toggleTruckMarkers);
 
-  $("#poposMap").on("change", function() {
-      togglePoposMarkers();
-  });
+  $("#poposMap").on("change", togglePoposMarkers);
 
-  $("#artMap").on("change", function() {
-      toggleArtMarkers();
-  });
+  $("#artMap").on("change", toggleArtMarkers);
 
 
 
@@ -338,18 +371,16 @@ $(document).ready(function() {
         } else {
           marker.setVisible(false);
         }
-      }
-      
+      } 
   }
 
   $("#searchForm").on("submit", submitSearch);
 
 
+
   ///////////////
   // Reset map //
   ///////////////
-
-
 
   function resetMap(evt) {
     $("#search").val("");
@@ -359,4 +390,4 @@ $(document).ready(function() {
   $("#resetMap").on("click", resetMap);
 
 
-});
+}
