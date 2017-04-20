e81813359c680e3acae564a6c1b62b8a2a652aef
Christine Urban <christineurbansf@gmail.com>
2017-04-20 20:56:23 +0000

    Fixed seed with correct data

--- seed.py
+++ seed.py
@@ -5,42 +5,37 @@ def example_data():
     """Create some sample data."""
 
     jane = User(email="abc@abc.com",
-                      password="abc",
-                      first_name="Jane",
-                      last_name="Doe")
+                password="abc",
+                first_name="Jane",
+                last_name="Doe")
 
     swell = Truck(truck_id=931094,
-                        name="Swell Cream & Coffee",
-                        address="2450 TARAVAL ST",
-                        cuisine="Ice cream: coffee: pastries",
-                        lat="37.7425503735592",
-                        lng="-122.492677082215")
+                  name="Swell Cream & Coffee",
+                  address="2450 TARAVAL ST",
+                  cuisine="Ice cream: coffee: pastries",
+                  lat="37.7425503735592",
+                  lng="-122.492677082215")
     
 
     mission = Popos(name="555 Mission St",
-                          address="555 Mission St",
-                          location="Western side of Building, street level.",
-                          popos_type="Plaza",
-                          hours="Open at all times",
-                          landscaping="Trees",
-                          art="Yes",
-                          food="Y",
-                          seating="Y",
-                          restrooms="None",
-                          description="This large plaza has several art and landscaping features. It is located in front of 555 Mission Streets and runs along the west side of the building. There are several distinct sitting areas with different styles of seating.",
-                          year="2008",
-                          lat="-122.39891",
-                          lng="37.7884")
+                    address="555 Mission St",
+                    location="Western side of Building, street level.",
+                    popos_type="Plaza",
+                    hours="Open at all times",
+                    description="This large plaza has several art and landscaping features. It is located in front of 555 Mission Streets and runs along the west side of the building. There are several distinct sitting areas with different styles of seating.",
+                    year="2008",
+                    lat="-122.39891",
+                    lng="37.7884")
 
 
     sansome = Art(address="343 Sansome",
-                        title="'\"Four Seasons\"' by Joan Brown",
-                        art_type="obelisk",
-                        medium="tile",
-                        location="roof garden",
-                        artist_link="http://en.wikipedia.org/wiki/Joan_Brown",
-                        lat="-122.401572",
-                        lng="37.793616")
+                  title="'\"Four Seasons\"' by Joan Brown",
+                  art_type="obelisk",
+                  medium="tile",
+                  location="roof garden",
+                  artist_link="http://en.wikipedia.org/wiki/Joan_Brown",
+                  lat="-122.401572",
+                  lng="37.793616")
 
 
     db.session.add_all([swell, mission, sansome, jane])
--- tests.py
+++ tests.py
@@ -76,31 +76,31 @@ class OuterSpacesTests(unittest.TestCase):
 
 
 
-# class OuterSpacesTestsDatabase(unittest.TestCase):
-#     """Flask tests that use the database."""
+class OuterSpacesTestsDatabase(unittest.TestCase):
+    """Flask tests that use the database."""
 
-#     def setUp(self):
-#         """Stuff to do before every test."""
+    def setUp(self):
+        """Stuff to do before every test."""
 
-#         # Get the Flask test client
-#         self.client = app.test_client()
+        # Get the Flask test client
+        self.client = app.test_client()
 
-#         # Show Flask errors that happen during tests
-#         app.config["TESTING"] = True
+        # Show Flask errors that happen during tests
+        app.config["TESTING"] = True
 
-#         # Connect to test database
-#         connect_to_db(app, "postgresql:///outerspacestest")
+        # Connect to test database
+        connect_to_db(app, "postgresql:///outerspacestest")
 
-#         # Create tables and add sample data
-#         db.create_all()
-#         example_data()
+        # Create tables and add sample data
+        db.create_all()
+        example_data()
 
 
-#     def tearDown(self):
-#         """Do at end of every test."""
+    def tearDown(self):
+        """Do at end of every test."""
 
-#         db.session.close()
-#         db.drop_all()
+        db.session.close()
+        db.drop_all()
 
 
 
